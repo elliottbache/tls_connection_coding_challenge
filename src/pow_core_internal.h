77300f132ba8d07df3dfe7f2b4a93199451679c3
Elliott Bache <elliott.bache@gmail.com>
2025-11-18 15:41:50 +0100

    Added header file for internal helper functions, allowing tests to use these functions while keeping them out of the public API.

---
+++ src/pow_core_internal.h
@@ -0,0 +1,23 @@
+#pragma once
+namespace pow_internal
+{
+    /** \brief Maximum concatenated input size (token + suffix) fed to SHA-256.
+
+        Increase if your token can be longer than this.
+        \see run_pow
+         */
+    constexpr size_t MAX_INPUT_SIZE = 256;
+
+    /** \brief Character set to be used for creating suffix.
+     */
+    const char charset[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+    /** \brief Length of charset */
+    const size_t charset_size = sizeof(charset) - 1;
+
+    // Determine suffix length so that keyspace ≥ 2^(difficulty * 4)
+    size_t determine_suffix_length(uint8_t difficulty);
+
+    void generate_counter_string(uint64_t counter, unsigned char *output, size_t output_length);
+
+    bool has_leading_zeros(const uint8_t *digest, int bits_required);
+}
