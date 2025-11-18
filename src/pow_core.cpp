8b4f8572c4dde87919682297cceeaf95f1f274dd
Elliott Bache <elliott.bache@gmail.com>
2025-11-18 12:17:51 +0100

    Moved internal helper functions to namespace pow_internal.  This way we can test them but they are not visible to the public.

--- src/pow_core.cpp
+++ src/pow_core.cpp
@@ -8,79 +8,89 @@
 #include <random>
 #include <cmath>
 #include "pow_core.h"
+#include "pow_core_internal.h"
+#include <cassert>
 
-// Determine suffix length so that keyspace ≥ 2^(difficulty * 4)
-size_t determine_suffix_length(uint8_t difficulty)
+namespace
 {
-    double required_bits = difficulty * 4;
-    double bits_per_char = std::log2(charset_size);
-    return static_cast<size_t>(std::ceil(required_bits / bits_per_char));
-}
 
-void generate_counter_string(uint64_t counter, unsigned char *output, size_t output_length)
-{
-    for (int i = output_length - 1; i >= 0; --i)
+    void pow_worker(const char *authdata, size_t auth_len, uint8_t difficulty,
+                    std::atomic<bool> &found, char *result,
+                    int thread_id, int total_threads, uint64_t base_counter, size_t suffix_length)
     {
-        output[i] = charset[counter % charset_size];
-        counter /= charset_size;
-    }
-}
+        unsigned char digest[SHA_DIGEST_LENGTH]{};
+        unsigned char suffix[suffix_length + 1]{};
+        alignas(64) unsigned char input[pow_internal::MAX_INPUT_SIZE] = {};
 
-bool has_leading_zeros(const uint8_t *digest, int bits_required)
-{
-    int full_bytes = bits_required / 8;
-    int remaining_bits = bits_required % 8;
+        int bits_required = difficulty * 4;
 
-    for (int i = 0; i < full_bytes; ++i)
-    {
-        if (digest[i] != 0)
-            return false;
-    }
+        uint64_t counter = base_counter + thread_id;
 
-    if (remaining_bits)
-    {
-        const uint8_t mask = static_cast<uint8_t>(0xFF << (8 - remaining_bits));
-        if ((digest[full_bytes] & mask) != 0)
-            return false;
-    }
-
-    return true;
-}
-
-void pow_worker(const char *authdata, size_t auth_len, uint8_t difficulty,
-                std::atomic<bool> &found, char *result,
-                int thread_id, int total_threads, uint64_t base_counter, size_t suffix_length)
-{
-    unsigned char digest[SHA_DIGEST_LENGTH]{};
-    unsigned char suffix[suffix_length + 1]{};
-    alignas(64) unsigned char input[MAX_INPUT_SIZE] = {};
-
-    int bits_required = difficulty * 4;
-
-    uint64_t counter = base_counter + thread_id;
-
-    while (!found.load())
-    {
-        generate_counter_string(counter, suffix, suffix_length);
-        counter += total_threads;
-
-        size_t input_len = auth_len + suffix_length;
-        if (input_len > MAX_INPUT_SIZE)
+        while (!found.load())
         {
-            throw std::runtime_error("Authdata is too long.");
+            pow_internal::generate_counter_string(counter, suffix, suffix_length);
+            counter += total_threads;
+
+            size_t input_len = auth_len + suffix_length;
+            if (input_len > pow_internal::MAX_INPUT_SIZE)
+            {
+                throw std::runtime_error("Authdata is too long.");
+            }
+            std::memcpy(input, authdata, auth_len);
+            std::memcpy(input + auth_len, suffix, suffix_length);
+
+            SHA1(input, input_len, digest);
+
+            if (pow_internal::has_leading_zeros(digest, bits_required))
+            {
+                std::memcpy(result, suffix, suffix_length);
+                result[suffix_length] = '\0';
+                found.store(true);
+                break;
+            }
         }
-        std::memcpy(input, authdata, auth_len);
-        std::memcpy(input + auth_len, suffix, suffix_length);
+    }
 
-        SHA1(input, input_len, digest);
+}
 
-        if (has_leading_zeros(digest, bits_required))
+namespace pow_internal
+{
+    // Determine suffix length so that keyspace ≥ 2^(difficulty * 4)
+    size_t determine_suffix_length(uint8_t difficulty)
+    {
+        double required_bits = difficulty * 4;
+        double bits_per_char = std::log2(pow_internal::charset_size);
+        return static_cast<size_t>(std::ceil(required_bits / bits_per_char));
+    }
+
+    void generate_counter_string(uint64_t counter, unsigned char *output, size_t output_length)
+    {
+        for (int i = output_length - 1; i >= 0; --i)
         {
-            std::memcpy(result, suffix, suffix_length);
-            result[suffix_length] = '\0';
-            found.store(true);
-            break;
+            output[i] = pow_internal::charset[counter % pow_internal::charset_size];
+            counter /= pow_internal::charset_size;
         }
+    }
+
+    bool has_leading_zeros(const uint8_t *digest, int bits_required)
+    {
+        int full_bytes = bits_required / 8;
+        int remaining_bits = bits_required % 8;
+
+        for (int i = 0; i < full_bytes; ++i)
+        {
+            if (digest[i] != 0)
+                return false;
+        }
+
+        if (remaining_bits)
+        {
+            const uint8_t mask = static_cast<uint8_t>(0xFF << (8 - remaining_bits));
+            if ((digest[full_bytes] & mask) != 0)
+                return false;
+        }
+
+        return true;
     }
 }
 
@@ -88,7 +98,7 @@ PowResult run_pow(const char *authdata, uint8_t difficulty)
 {
     size_t auth_len = std::strlen(authdata);
 
-    size_t suffix_length = determine_suffix_length(difficulty);
+    size_t suffix_length = pow_internal::determine_suffix_length(difficulty);
 
     std::random_device rd;
     std::mt19937_64 gen(rd());
