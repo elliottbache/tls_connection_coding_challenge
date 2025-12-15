0351629fbb808afe175095225d5e362de546d8a8
Elliott Bache <elliott.bache@gmail.com>
2025-12-15 17:05:44 +0100

    Moved receive_message to a new module called protocol.py.

--- src/tests/test_server.py
+++ src/tests/test_server.py
@@ -11,11 +11,6 @@ from src import server
 
 
 # helpers
-class FakeSock:
-    def recv(self, n):
-        return "hello\n"  # <-- str, not bytes
-
-
 class FakeContext:
     def __init__(self):
         self.purpose = None
@@ -66,56 +61,6 @@ class TestSendMessage:
         err = server.send_message(payload, s1)
         _ = s2.recv(1024)
         assert err is None
-
-
-class TestReceiveMessage:
-    def test_receive_message(self, socket_pair):
-        s1, s2 = socket_pair
-        message_to_receive = b"HELLOBACK\n"
-
-        _ = s1.sendall(message_to_receive)
-        received_message = server.receive_message(s2)
-
-        assert received_message == "HELLOBACK"
-
-    def test_receive_message_non_utf(self, socket_pair, readout):
-        s1, s2 = socket_pair
-        message_to_receive = "æ".encode("cp1252")
-
-        _ = s1.sendall(message_to_receive)
-        with pytest.raises(ValueError, match=r"Receive failed.  Invalid UTF-8:"):
-            server.receive_message(s2)
-
-    def test_receive_message_no_newline(self, socket_pair, readout):
-        s1, s2 = socket_pair
-        s1.settimeout(0.1)
-        message_to_receive = b"HELLOBACK"
-
-        _ = s1.sendall(message_to_receive)
-        with pytest.raises(Exception, match=r"Receive failed:"):
-            server.receive_message(s2)
-
-    def test_receive_empty_message(self, socket_pair, readout):
-        s1, s2 = socket_pair
-        s1.close()
-
-        with pytest.raises(
-            ValueError, match=r"Receive failed.  Received empty string."
-        ):
-            server.receive_message(s2)
-
-    def test_receive_non_bytes(self, socket_pair, readout):
-        sock = FakeSock()
-        with pytest.raises(TypeError, match=r"Receive failed.  Unexpected type:"):
-            server.receive_message(sock)
-
-    def test_receive_long_line(self, socket_pair, readout):
-        s1, s2 = socket_pair
-        message_to_receive = bytes([3]) * 1001 + b"\n"
-
-        _ = s1.sendall(message_to_receive)
-        with pytest.raises(ValueError, match=r"Line too long"):
-            server.receive_message(s2)
 
 
 @pytest.fixture
