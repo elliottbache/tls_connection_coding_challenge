c9bb288ce7e40a27702f573b05d8cc39e49c1a8a
Elliott Bache <elliott.bache@gmail.com>
2025-12-16 14:36:51 +0100

    Minor fixes to pass CodeQL on GitHub Actions.

--- docs/conf.py
+++ docs/conf.py
@@ -59,10 +59,8 @@ html_static_path = ["_static"]
 
 # add C++ documentation
 # Sphinx (docs/conf.py)
-breathe_projects = {"tls-connection-coding-challenge": "_build/doxygen/xml"}
 breathe_default_project = "tls-connection-coding-challenge"
 
 DOCS = pathlib.Path(__file__).parent
 BREATHE_XML = DOCS / "_build" / "doxygen" / "xml"
-
 breathe_projects = {"tls-connection-coding-challenge": str(BREATHE_XML)}
