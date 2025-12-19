2664682c7ce6c7b2825abe969482d76274c19c90
Elliott Bache <elliottbache@gmail.com>
2025-12-19 09:54:11 +0100

    feat(pow_challenge): started using SHA256_Update.
    Before the SHA256(...) was used to calculate hash.  Now, SHA256_Init, SHA256_Update, SHA256_Final.

--- cpp/pow_core.cpp
+++ cpp/pow_core.cpp
@@ -25,6 +25,10 @@ namespace
         const int bits_required = difficulty * 4;
         uint64_t counter = base_counter + thread_id;
 
+        SHA_CTX sha_context_base;
+        SHA256_Init(&sha_context_base);
+        SHA256_Update(&sha_context_base, token, auth_len);
+
         while (!found.load(std::memory_order_acquire))
         {
             pow_internal::generate_counter_string(counter, suffix.data(), suffix_length);
@@ -37,7 +41,9 @@ namespace
             std::memcpy(input, token, auth_len);
             std::memcpy(input + auth_len, suffix.data(), suffix_length);
 
-            SHA256(input, input_len, digest);
+            SHA_CTX sha_context = sha_context_base;
+            SHA256_Update(&sha_context, suffix.data(), suffix_length);
+            SHA256_Final(digest, &sha_context);
 
             if (pow_internal::has_leading_zeros(digest, bits_required))
             {
